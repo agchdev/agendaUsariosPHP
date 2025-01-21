8f6054d42e7bb865c45b7a5557624cfa46a40493
agchdev <alex.chinchi02@gmail.com>
2025-01-22 00:25:31 +0100

    cambios

--- vista/amigos.php
+++ vista/amigos.php
@@ -10,22 +10,23 @@
     <section>
         <h2>Amigos de <?php if(isset($usuario)) echo $usuario ?></h2>
         <?php
-            echo count($amigosUsu);
             if (count($amigosUsu) == 0) {
                 echo "NO TIENE AMIGOS ACTUALMENTE";
             }else{
                 echo "<form action='index.php' method='post'>"; 
                 echo "<table>";
                     echo "<tr><th>NOMBRE</th><th>APELLIDOS</th><th>FECHA NACIMIENTO</th></tr>";
-                    echo "<tr>";
+                    
                     foreach ($amigosUsu as $amig) {
+                        echo "<tr>";
                         echo "<input type='hidden' name='id_usuario' value='" . $amig["id_usuario"] . "'>";
                         echo "<input type='hidden' name='id' value='" . $amig["id"] . "'>"; 
-                        echo "<td>" . $amig["nombre"] . "a</td>";
-                        echo "<td>" . $amig["apellidos"] . "a</td>";
-                        echo "<td>" . $amig["fecha_nac"] . "a</td>";
+                        echo "<td>" . $amig["nombre"] . "</td>";
+                        echo "<td>" . $amig["apellidos"] . "</td>";
+                        echo "<td>" . $amig["fecha_nac"] . "</td>";
+                        echo "</tr>";
                     }
-                    echo "</tr>";
+                    
                 echo "</table>";
                 echo "</form>";
             }
